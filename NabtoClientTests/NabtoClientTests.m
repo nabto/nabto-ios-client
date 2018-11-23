329997700e7621d2fc01582c7dfdb72be5688bd5
Ulrik Gammelby <ug@nabto.com>
2018-11-23 20:43:34 +0100

    update tests to new client interface

--- NabtoClientTests/NabtoClientTests.m
+++ NabtoClientTests/NabtoClientTests.m
@@ -26,90 +26,69 @@
 }
 
 - (void)testObjcWrapper {
-    XCTAssertEqual([[NabtoClient instance] nabtoStartup], NABTO_OK);
-    XCTAssertEqual([[NabtoClient instance] nabtoOpenSessionGuest], NABTO_OK);
+    XCTAssertEqual([[NabtoClient instance] nabtoStartup], NCS_OK);
+    XCTAssertEqual([[NabtoClient instance] nabtoOpenSessionGuest], NCS_OK);
     char* result;
     size_t len;
     char* mimeType;
-    nabto_status_t status;
+    NabtoClientStatus status;
     status = [[NabtoClient instance] nabtoFetchUrl:@"nabto://demo.nabto.net/wind_speed.json?" withResultBuffer:&result resultLength:&len mimeType:&mimeType];
-    if (status == NABTO_OK) {
+    if (status == NCS_OK) {
         NSData *data = [NSData dataWithBytes:result length:len];
         NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
         NSLog(@"fetchUrl finished with result: %@", str);
         XCTAssertTrue([str containsString:@"speed_m_s"]);
-        nabtoFree(result);
-        nabtoFree(mimeType);
+        [[NabtoClient instance] nabtoFree:result];
+        [[NabtoClient instance] nabtoFree:mimeType];
     } else {
-        NSLog(@"fetchUrl finished with status %d", status);
+        NSLog(@"fetchUrl finished with status %zd", (long)status);
     }
-    XCTAssertEqual(status, NABTO_OK);
+    XCTAssertEqual(status, NCS_OK);
 }
 
 - (void)testObjcWrapperRpc {
-    XCTAssertEqual([[NabtoClient instance] nabtoStartup], NABTO_OK);
-    XCTAssertEqual([[NabtoClient instance] nabtoOpenSessionGuest], NABTO_OK);
+    XCTAssertEqual([[NabtoClient instance] nabtoStartup], NCS_OK);
+    XCTAssertEqual([[NabtoClient instance] nabtoOpenSessionGuest], NCS_OK);
     char* json;
-    nabto_status_t status;
+    NabtoClientStatus status;
     NSString* interfaceXml = @"<unabto_queries><query name='wind_speed.json' id='2'><request></request><response format='json'><parameter name='rpc_speed_m_s' type='uint32'/></response></query></unabto_queries>";
     char* errorMsg;
-    if ([[NabtoClient instance] nabtoRpcSetDefaultInterface:interfaceXml withErrorMessage:&errorMsg] != NABTO_OK) { XCTAssertTrue(false);
-    };
+    XCTAssertEqual([[NabtoClient instance] nabtoRpcSetDefaultInterface:interfaceXml withErrorMessage:&errorMsg], NCS_OK);
     status = [[NabtoClient instance] nabtoRpcInvoke:@"nabto://demo.nabto.net/wind_speed.json?" withResultBuffer:&json];
-    if (status == NABTO_OK) {
+    if (status == NCS_OK) {
         NSLog(@"rpcInvoke finished with result: %s", json);
         NSString *str = [NSString stringWithUTF8String:json];
         XCTAssertTrue([str containsString:@"rpc_speed_m_s"]);
-        nabtoFree(json);
+        [[NabtoClient instance] nabtoFree:json];
     }
-    XCTAssertEqual(status, NABTO_OK);
+    XCTAssertEqual(status, NCS_OK);
 }
 
 - (void)testObjCWrapperFromDocs {
-    if ([[NabtoClient instance] nabtoStartup] != NABTO_OK) {
+    if ([[NabtoClient instance] nabtoStartup] != NCS_OK) {
         // handle error
     }
-    if ([[NabtoClient instance] nabtoOpenSessionGuest] != NABTO_OK) {
+    if ([[NabtoClient instance] nabtoOpenSessionGuest] != NCS_OK) {
         // handle error
     }
     
     NSString* interfaceXml = @"<unabto_queries><query name='wind_speed.json' id='2'><request></request><response format='json'><parameter name='rpc_speed_m_s' type='uint32'/></response></query></unabto_queries>";
     
     char* errorMsg;
-    if ([[NabtoClient instance] nabtoRpcSetDefaultInterface:interfaceXml withErrorMessage:&errorMsg] != NABTO_OK) {
+    if ([[NabtoClient instance] nabtoRpcSetDefaultInterface:interfaceXml withErrorMessage:&errorMsg] != NCS_OK) {
         // handle error
     }
     
     char* json;
-    nabto_status_t status = [[NabtoClient instance] nabtoRpcInvoke:@"nabto://demo.nabto.net/wind_speed.json?" withResultBuffer:&json];
-    if (status == NABTO_OK) {
+    NabtoClientStatus status = [[NabtoClient instance] nabtoRpcInvoke:@"nabto://demo.nabto.net/wind_speed.json?" withResultBuffer:&json];
+    if (status == NCS_OK) {
         NSLog(@"rpcInvoke from docs finished with result: %s", json);
-        nabtoFree(json);
+        [[NabtoClient instance] nabtoFree:json];
     }
     
     [[NabtoClient instance] nabtoShutdown];
 }
 
-- (void)testNativeSdk {
-    XCTAssertEqual(nabtoStartup(NULL), NABTO_OK);
-    nabto_handle_t session;
-    XCTAssertEqual(nabtoOpenSession(&session, "guest", "123456"), NABTO_OK);
-    char* result;
-    size_t len;
-    char* mimeType;
-    nabto_status_t status;
-    status = nabtoFetchUrl(session, "nabto://demo.nabto.net/wind_speed.json?", &result, &len, &mimeType);
-    if (status == NABTO_OK) {
-        NSData *data = [NSData dataWithBytes:result length:len];
-        NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-        NSLog(@"fetchUrl finished with result: %@", str);
-        XCTAssertTrue([str containsString:@"speed_m_s"]);
-        nabtoFree(result);
-        nabtoFree(mimeType);
-    } else {
-        NSLog(@"fetchUrl finished with status %d", status);
-    }
-    XCTAssertEqual(status, NABTO_OK);
-}
 
 @end
+
