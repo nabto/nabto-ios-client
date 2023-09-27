0bcb399d243c5f4ee5fbca1f0b9cd8c06810ddf9
Ulrik Gammelby <ug@nabto.com>
2023-09-27 14:23:12 +0200

    remove build script as pod is now distributed as source with dependency to NabtoAPI xcframework managed through cocoapods; bump version (use new NabtoAPI)

--- build.sh
+++ build.sh
@@ -1,49 +1,47 @@
 #!/bin/bash
 
-DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+set -e
 
-if [ $# != 2 ]; then
-    echo "usage: $0 <build dir> <output dir>"
-    exit 1
-fi
+pod install
 
-IOS_FW_BUILD=$1
-IOS_FW_ARTIFACTS=$2
-IOS_FW_PROJECT_NAME=NabtoClient
-IOS_FW_BUILD_ROOT=$DIR
+CONFIG=Release
+PROJECT_NAME=NabtoClient
+BUILD_ROOT=`pwd`
+BUILD=$BUILD_ROOT/build-$CONFIG
+ARTIFACTS=$BUILD_ROOT/dist
+PRODUCT_PATH=Products/Library/Frameworks/${PROJECT_NAME}.framework
+WORKSPACE=${PROJECT_NAME}.xcworkspace
 
-function sdk {
-    echo "================ iOS: Build SDK Framework Bundle ================"
-    set -e
-    
-    rm -rf $IOS_FW_BUILD
-    rm -rf $IOS_FW_ARTIFACTS
-    mkdir -p $IOS_FW_BUILD
-    mkdir -p $IOS_FW_ARTIFACTS
+rm -rf $ARTIFACTS
+mkdir -p $BUILD
+mkdir -p $ARTIFACTS
 
-    cd $DIR
-    pod install
+# iOS
+xcodebuild clean archive \
+    -workspace $WORKSPACE \
+    -scheme "${PROJECT_NAME}" \
+    -archivePath $BUILD/ios.xcarchive \
+    -configuration $CONFIG \
+    -sdk iphoneos \
+    SKIP_INSTALL=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-    # bitcode options from https://stackoverflow.com/questions/33106117/how-do-i-make-fat-framework-with-bitcode-option and https://medium.com/@heitorburger/static-libraries-frameworks-and-bitcode-6d8f784478a9
+# iOS sim
+xcodebuild clean archive \
+    -workspace $WORKSPACE \
+    -scheme "${PROJECT_NAME}" \
+    -archivePath $BUILD/ios-sim.xcarchive \
+    -configuration $CONFIG \
+    -sdk iphonesimulator \
+    SKIP_INSTALL=NO \
+    BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-    xcodebuild -workspace NabtoClient.xcworkspace -UseModernBuildSystem=NO -scheme "${IOS_FW_PROJECT_NAME}" ONLY_ACTIVE_ARCH=NO -configuration Release -sdk iphoneos BUILD_DIR=${IOS_FW_BUILD} BUILD_ROOT="${IOS_FW_BUILD_ROOT}" OTHER_CFLAGS="-fembed-bitcode" BITCODE_GENERATION_MODE=bitcode clean build
-    xcodebuild -workspace NabtoClient.xcworkspace -UseModernBuildSystem=NO -scheme "${IOS_FW_PROJECT_NAME}" ONLY_ACTIVE_ARCH=NO -configuration Release -sdk iphonesimulator BUILD_DIR=${IOS_FW_BUILD} BUILD_ROOT="${IOS_FW_BUILD_ROOT}" OTHER_CFLAGS="-fembed-bitcode" BITCODE_GENERATION_MODE=bitcode clean build
-    
-    IOS_FW_DEVICE_DIR="${IOS_FW_BUILD}/Release-iphoneos/${IOS_FW_PROJECT_NAME}.framework"
-    IOS_FW_SIM_DIR="${IOS_FW_BUILD}/Release-iphonesimulator/${IOS_FW_PROJECT_NAME}.framework"
-    
-    # pick one to use for base structure (everything interesting is identical)
-    cp -R ${IOS_FW_DEVICE_DIR} ${IOS_FW_ARTIFACTS}
+xcodebuild -create-xcframework \
+           -framework "$BUILD/ios.xcarchive/$PRODUCT_PATH" \
+           -framework "$BUILD/ios-sim.xcarchive/$PRODUCT_PATH" \
+           -output "$ARTIFACTS/$PROJECT_NAME.xcframework"
 
-    # build universal binary
-    lipo -create ${IOS_FW_DEVICE_DIR}/${IOS_FW_PROJECT_NAME} ${IOS_FW_SIM_DIR}/${IOS_FW_PROJECT_NAME} -output ${IOS_FW_ARTIFACTS}/${IOS_FW_PROJECT_NAME}.framework/${IOS_FW_PROJECT_NAME}
-    
-    cp LICENSE ${IOS_FW_ARTIFACTS}/${IOS_FW_PROJECT_NAME}.framework
-    cp ${DIR}/NabtoClient/NabtoClient.h ${IOS_FW_ARTIFACTS}/${IOS_FW_PROJECT_NAME}.framework/Headers
+cd $ARTIFACTS
+cp ../LICENSE ../README.md ${PROJECT_NAME}.xcframework
 
-    cd $IOS_FW_ARTIFACTS
-    zip -r ${IOS_FW_PROJECT_NAME}.framework.zip ${IOS_FW_PROJECT_NAME}.framework    
-}
-
-sdk
-
+zip -r ${PROJECT_NAME}.xcframework.zip ${PROJECT_NAME}.xcframework
