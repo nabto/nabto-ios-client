53abd333f53f641e65f4fed25b47b773cd47fd31
nabto <nabto@mac>
2026-06-15 14:35:04 +0200

    updated Nabto API to v4.9.6

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NabtoAPI",
-            url: "https://downloads.nabto.com/assets/nabto-libs/v4.9.4/NabtoAPI.xcframework.zip",
-            checksum: "b66cfc460ca85289c071251bf10d4b09db6f87ce2a5b685e8ec5cba446225456"
+            url: "https://downloads.nabto.com/assets/nabto-libs/v4.9.6/NabtoAPI.xcframework.zip",
+            checksum: "e9838e4af1ac1ba1f681dfc472da1293739e5ccf933f59f1363723c3a8ce10b1"
         ),
         .target(
             name: "NabtoClient",
